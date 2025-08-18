0fc7ba9cd16faf6029ca59576a99792a56e13dcb
Levelasquez15 <lewis.velasquezwatts@unicolombo.edu.co>
2025-08-18 00:23:51 -0500

    Calculadora calidad

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    public int restar(int a, int b) {
+        return a - b;
+    }
+    public int multiplicar(int a, int b) {
+        if (a == 0 || b == 0) return 0;
+        return a * b;
+    }
+    public int dividir(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }
+        return a / b;
+
+    }
+
+
+}
---
+++ src/test/java/Calculadora_cal.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Calculadora_cal {
+
+    Calculadora calidad = new Calculadora();
+
+
+    @Test
+    void sumar() {
+        assertEquals(5, calidad.sumar(2, 3));
+    }
+    @Test
+    void sumarNumeros_negativos() {
+        assertEquals(-5, calidad.sumar(-2, -3));
+    }
+
+    @Test
+    void restar() {
+        assertEquals(1, calidad.restar(3, 2));
+
+    }
+    @Test
+    void restarResultado_negativo() {
+        assertEquals(-1, calidad.restar(2, 3));
+    }
+
+    @Test
+    void multiplicar() {
+        assertEquals(6, calidad.multiplicar(2, 3));
+
+    }
+    @Test
+    void multiplicarPorCero() {
+        assertEquals(0, calidad.multiplicar(0, 5));
+        assertEquals(0, calidad.multiplicar(5, 0));
+    }
+
+    @Test
+    void dividir() {
+        assertEquals(2, calidad.dividir(6, 3));
+    }
+    @Test
+    void dividirPorCeroDebeLanzarExcepcion() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calidad.dividir(10, 0);
+        });
+    }
+
+
+}
