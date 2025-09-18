aaac88e8f41202b1b48e41ca96dd5e29b8a9e11c
Levelasquez15 <lewis.velasquezwatts@unicolombo.edu.co>
2025-09-17 22:43:11 -0500

    hacienod lo del taller

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class Calculadora_cal {
+class CalculadoraTest {
 
     Calculadora calidad = new Calculadora();
 
